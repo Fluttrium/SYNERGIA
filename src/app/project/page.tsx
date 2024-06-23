50e56d96b8e87d5ab94734ba3011d6f740b47c50
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-06-23 17:30:30 +0300

    Cделал страницу проекты

---
+++ src/app/project/page.tsx
@@ -0,0 +1,14 @@
+import Project from "@/components/Project";
+
+
+
+export const metadata = {
+  title: "Home - Open PRO",
+  description: "Page description",
+};
+export default function Home() {
+  return <>
+  <Project/>
+  
+  </>;
+}
