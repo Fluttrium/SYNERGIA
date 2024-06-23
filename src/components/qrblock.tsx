15983dd3a26d381a3612e86dd4a4dcc3e8bd57e2
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-06-23 18:20:41 +0300

    буклеты сделаны, необходимо добавить из в раздел услуги в сфере миграции

--- src/components/qrblock.tsx
+++ src/components/qrblock.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 
 export default function QrBlock() {
   return (
-    <section className="relative flex bg-white h-max z-1 w-full">
+    <section className="relative flex bg-white h-max z-1 w-full py-48">
       <div className="max-w-screen-lg mx-auto">
+      <h2 className="text-4xl font-bold pb-10 text-center">Полезная информация для иностранных граждан</h2>
         <ul className="font-bold text-black">
           <li className="hover:text-black transition duration-300">
-            <a href="/uzbek" className="block text-3xl hover:bg-gray-200 p-2 rounded-md">
+            <a href="/rus" className="block text-3xl hover:bg-gray-200 p-2 rounded-md">
               Буклеты на русском языке
             </a>
           </li>
@@ -17,8 +18,8 @@ export default function QrBlock() {
             </a>
           </li>
           <li className="hover:text-black transition duration-300">
-            <a href="/tadjik" className="block text-3xl hover:bg-gray-200 p-2 rounded-md">
-              Буклеты на таджикском языке (бо забони точики)
+            <a href="/uzbek" className="block text-3xl hover:bg-gray-200 p-2 rounded-md">
+              Буклеты на узбекском языке (o'zbek tilida)
             </a>
           </li>
         </ul>
