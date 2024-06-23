fb43765797ec56fd6390e4a04465a228d987fe37
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-06-23 15:07:22 +0300

    Страница Символы

---
+++ src/app/symbol/page.tsx
@@ -0,0 +1,14 @@
+import Symbol from "@/components/Symbol";
+
+
+
+export const metadata = {
+  title: "Home - Open PRO",
+  description: "Page description",
+};
+export default function Home() {
+  return <>
+  <Symbol/>
+  
+  </>;
+}
